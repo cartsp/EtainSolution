d27368c95274d854cb700b05fd758de1fc01569d
liam_ <liam_@DESKTOP-R9N97O1>
2020-01-21 07:46:38 +0000

    Clean up

--- WeatherForecastEtain.MVC/Controllers/HomeController.cs
+++ WeatherForecastEtain.MVC/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using System.Threading.Tasks;
-using WeatherForecastEtain.Core.Interfaces;
 using WeatherForecastEtain.MVC.Models;
 
 namespace WeatherForecastEtain.MVC.Controllers
@@ -11,38 +9,17 @@ namespace WeatherForecastEtain.MVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> logger;
-        private readonly IWeatherForcastService weatherForcastService;
 
-        public HomeController(ILogger<HomeController> logger, IWeatherForcastService weatherForcastService)
+        public HomeController(ILogger<HomeController> logger)
         {
             this.logger = logger;
-            this.weatherForcastService = weatherForcastService;
-        }
+         }
 
         public async Task<IActionResult> Index()
         {
             logger.LogInformation("Index page loaded");
 
             return await Task.Run(() => View());
-        }
-
-        [Authorize]
-        public async Task<IActionResult> Weather()
-        {
-            var weatherResults = await weatherForcastService.GetNextFiveDayForecast("Belfast");
-            
-            logger.LogInformation("Weather page loaded");
-            
-            return View(weatherResults.Forecasts);
-        }
-
-        [Authorize]
-        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
-        public async Task<IActionResult> GetWeather()
-        {
-            var weatherResults = await weatherForcastService.GetNextFiveDayForecast("Belfast");
-
-            return Json(weatherResults.Forecasts);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
