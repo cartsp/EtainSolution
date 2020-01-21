264edc66884a3c551609ca0a1814b65f59f8e94d
liam_ <liam_@DESKTOP-R9N97O1>
2020-01-21 01:43:51 +0000

    Error handling and generic naming

---
+++ Weather-Forcast-Etain.Core/Exceptions/AccountLoginException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WeatherForecastEtain.Core.Exceptions
+{
+    public class AccountLoginException : Exception
+    {
+        public AccountLoginException(string message) : base(message)
+        {
+        }
+
+        public AccountLoginException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- Weather-Forcast-Etain.Infrastructure/Services/AccountService.cs
+++ Weather-Forcast-Etain.Infrastructure/Services/AccountService.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 using WeatherForecastEtain.Core.Entities;
+using WeatherForecastEtain.Core.Exceptions;
 using WeatherForecastEtain.Core.Interfaces;
 
 namespace WeatherForecastEtain.Infrastructure.Services
@@ -7,15 +9,26 @@ namespace WeatherForecastEtain.Infrastructure.Services
     public class AccountService : IAccountService
     {
         private readonly IUserRepository userRepository;
+        private readonly ILogger<AccountService> logger;
 
-        public AccountService(IUserRepository userRepository)
+        public AccountService(IUserRepository userRepository, ILogger<AccountService> logger)
         {
+            this.logger = logger;
             this.userRepository = userRepository;
         }
 
         public async Task<User> Login(string username, string password)
         {
-            return await userRepository.GetUserByUsernameAndPasswordAsync(username, password);
+            try
+            {
+                return await userRepository.GetUserByUsernameAndPasswordAsync(username, password);
+            }
+            catch (System.Exception ex)
+            {
+                logger.LogError(ex.ToString());
+
+                throw new AccountLoginException(ex.ToString());
+            }
         }
     }
 }
